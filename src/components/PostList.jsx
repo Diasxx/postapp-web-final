db0a9026b58fba87f3df4af5689b2ce53d98caad
Dias Shukirbay <119118620+Diasxx@users.noreply.github.com>
2023-12-14 22:59:31 +0600

    added animation
    animation to entering post and deleting post

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PostItem from "./PostItem";
+import {CSSTransition, TransitionGroup} from "react-transition-group";
 
 const PostList = ({posts,title,remove}) => {
 
@@ -15,8 +16,18 @@ const PostList = ({posts,title,remove}) => {
         <div>
             <h1 style={{textAlign:'center'}}>{title}</h1>
 
-            {posts.map((posts,index) =>
-                <PostItem remove={remove} number={index+1} post={posts}/>)}
+            <TransitionGroup>
+                {posts.map((post,index) =>
+                    <CSSTransition
+                        key={post.id}
+                        timeout={500}
+                        classNames="post"
+                    >
+                    <PostItem remove={remove} number={index+1} post={post} key={post.id}/>
+                    </CSSTransition>
+                )}
+            </TransitionGroup>
+
         </div>
     );
 };
