d6b6411985dd774db7f9c9486df41e3ebe642e10
Dias Shukirbay <119118620+Diasxx@users.noreply.github.com>
2023-12-15 18:07:18 +0600

    Pagination

--- src/App.js
+++ src/App.js
@@ -9,20 +9,36 @@ import {usePosts} from "./hooks/usePosts";
 import PostService from "./API/PostService";
 import Loader from "./components/UI/Loader/Loader";
 import {useFetching} from "./hooks/useFetching";
+import {getPageCount, getPagesArray} from "./utils/pages";
+import Pagination from "./components/UI/pagination/Pagination";
 function App() {
 
     const [posts,setPosts] = useState([])
+
     const [filter,setFilter] = useState({sort:'',query:''})
+
     const [modal,setModal] = useState(false);
+
+    const [totalPages,setTotalPages]=useState(0)
+
+    const [limit,setLimit]=useState(10)
+
+    const [page,setPage]=useState(1)
+
     const sortedAndSearchedPosts = usePosts(posts,filter.sort,filter.query);
-    const [fetchPosts,isPostsLoading,postError]=useFetching(async ()=>{
-        const posts = await PostService.getALl()
-        setPosts(posts)
+
+
+
+    const [fetchPosts,isPostsLoading,postError]=useFetching(async (limit,page)=>{
+        const response = await PostService.getALl(limit,page)
+        setPosts(response.data)
+        const totalCount = response.headers['x-total-count']
+        setTotalPages(getPageCount(totalCount,limit))
     })
 
     useEffect(() => {
-        fetchPosts()
-    }, []);
+        fetchPosts(limit,page)
+    }, [page]);
     const createPost=(newPost)=>{
         setPosts([...posts,newPost])
         setModal(false)
@@ -31,8 +47,12 @@ function App() {
         setPosts(posts.filter(p => p.id !== post.id))
     }
 
+    const changePage=()=>{
+        setPage(page)
+        fetchPosts(limit,page)
+    }
 
-  return (
+    return (
     <div className="App">
         <MyButton style={{marginTop:30}} onClick={()=>setModal(true)}>
             Create Post
@@ -47,7 +67,6 @@ function App() {
             filter={filter}
             setFilter={setFilter}
         />
-
         {
             postError &&
             <h1>Error occurred ${postError}</h1>
@@ -58,8 +77,14 @@ function App() {
             :<PostList remove={removePost} posts={sortedAndSearchedPosts} title="High level language"/>
         }
 
+        <Pagination
+            page={page}
+            setPage={setPage}
+            totalPages={totalPages}
+        />
+
     </div>
-  );
+    );
 }
 
 export default App;
