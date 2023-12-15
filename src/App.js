6f9185a5e3ee7d89a5b257e7b47c5d2a6ca8318a
Dias Shukirbay <119118620+Diasxx@users.noreply.github.com>
2023-12-15 10:36:11 +0600

    Loading Animation
    Added Loading Animation

--- src/App.js
+++ src/App.js
@@ -6,14 +6,15 @@ import PostFiler from "./components/PostFiler";
 import MyModal from "./components/UI/MyModal/MyModal";
 import MyButton from "./components/UI/button/MyButton";
 import {usePosts} from "./hooks/usePosts";
-import axios from "axios";
 import PostService from "./API/PostService";
+import Loader from "./components/UI/Loader/Loader";
 function App() {
 
     const [posts,setPosts] = useState([])
     const [filter,setFilter] = useState({sort:'',query:''})
     const [modal,setModal] = useState(false);
     const sortedAndSearchedPosts = usePosts(posts,filter.sort,filter.query);
+    const [isPostsLoading,setIsPostsLoading]= useState(false);
 
     useEffect(() => {
         fetchPosts()
@@ -23,8 +24,10 @@ function App() {
         setModal(false)
     }
     async function fetchPosts(){
+        setIsPostsLoading(true);
         const posts = await PostService.getALl()
         setPosts(posts)
+        setIsPostsLoading(false);
     }
     const removePost = (post)=>{
         setPosts(posts.filter(p => p.id !== post.id))
@@ -49,7 +52,10 @@ function App() {
             filter={filter}
             setFilter={setFilter}
         />
-        <PostList remove={removePost} posts={sortedAndSearchedPosts} title="High level language"/>
+        {isPostsLoading
+            ?<div style={{display:'flex',justifyContent:'center',marginTop:50}}><Loader/></div>
+            :<PostList remove={removePost} posts={sortedAndSearchedPosts} title="High level language"/>
+        }
 
     </div>
   );
