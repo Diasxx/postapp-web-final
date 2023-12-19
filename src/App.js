68a6c7e91f0f26349a82a2381ce196aca14dac68
Dias Shukirbay <119118620+Diasxx@users.noreply.github.com>
2023-12-19 21:43:18 +0600

    Dividing styles
    Dividing styles  into Login,  PostId,Profile,About and so on

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React, {useEffect, useState} from 'react';
 import './styles/App.css';
+import './styles/Profile.css';
+import './styles/About.css';
+import './styles/Login.css';
+import './styles/PostId.css';
 import { BrowserRouter } from 'react-router-dom';
 import Navbar from "./components/UI/Navbar/Navbar";
 import AppRouter from "./components/AppRouter";
